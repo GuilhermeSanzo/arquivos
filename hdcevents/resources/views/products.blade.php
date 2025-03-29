5308782f36e5d62fab13da9bd97d983b59374e08
Guilherme Souza <81893267+GuilhermeSanzo@users.noreply.github.com>
2025-03-29 14:48:31 -0300

    Resgatando parâmetros de URL

--- hdcevents/resources/views/products.blade.php
+++ hdcevents/resources/views/products.blade.php
@@ -3,5 +3,11 @@
 @section('title', 'Produtos')
 
 @section('content')
+
     <h1>Tela de Produtos</h1>        
+
+    @if($busca != '')
+        <p>Você está buscando por: {{ $busca }}</p>
+    @endif
+
 @endsection
