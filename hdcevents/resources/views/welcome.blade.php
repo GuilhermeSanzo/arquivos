822a7ce7f5de180fc7874684cfcce428302682e0
Guilherme Souza <81893267+GuilhermeSanzo@users.noreply.github.com>
2025-03-31 23:29:27 -0300

    Upload de imagens com Laravel

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
             <div class="card col-md-3">
-                <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                 <div class="class-body">
                     <p class="card-date">31/03/2025</p>
                     <h5 class="card-title">{{ $event->title }}</h5>
