c5e9db95c76e66de130dd1eb0d7e949498b4385e
Guilherme Souza <81893267+GuilhermeSanzo@users.noreply.github.com>
2025-04-01 15:07:34 -0300

    Exibindo eventos do usuário

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -66,8 +66,9 @@ class EventController extends Controller
     public function dashboard() {
         $user = auth()->user();
         $events = $user->events;
+        $eventsAsParticipant = $user->eventsAsParticipant;
 
-        return view('events.dashboard', ['events' => $events]);
+        return view('events.dashboard', ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]);
     }
 
     public function destroy($id) {
@@ -77,7 +78,12 @@ class EventController extends Controller
     }
 
     public function edit($id) {
+        $user = auth()->user();
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
     }
